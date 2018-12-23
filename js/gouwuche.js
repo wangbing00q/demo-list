d29dc0063998c13b3c94752fb21c2a953de014d8
王冰 <398120285@qq.com>
2018-12-23 13:41:54 +0800

    购物车Demo

---
+++ js/gouwuche.js
@@ -0,0 +1,285 @@
+// var data=[{shop:"薛文鹏的镐把子店",img:"images/TB2QBzwmFXXXXbCXXXXXXXXXXXX_!!21171835.jpg_80x80.jpg",cont:"加厚隔热防晒现代美式纯色窗帘布全遮光定制卧室客厅落地飘窗成品",info1:"尺寸：宽2高2.6米 布袋挂钩一片",info2:"浅灰色[暮光]",pride1:"280.00",pride2:"162.40",number:"1"},
+// {shop:"加多宝的袜子店",img:"images/TB2Q7i.vSVmpuFjSZFFXXcZApXa_!!2785160894.jpg_80x80.jpg",cont:"加厚美式棉麻全遮光窗帘隔音隔热客厅卧室窗帘布北欧纯色现代简约",info1:"尺寸：不加工/每米价格",info2:"黄色",pride1:"132.00",pride2:"66",number:"10"},
+// {shop:"农夫山泉的豆腐乳店",img:"images/TB14qtKnMmTBuNjy1XbL6SMrVXa_80x80.jpg",cont:"【可领用100元单品券】Meizu/魅族 魅族 15 PLUS 全网通公开版4G",info1:"网络类型：4G全网通",info2:"机身颜色：玄武灰",info3:"套餐类型：官方标配",info4:"存储容量：6+64GB",pride2:"2498.00",number:"1"},
+// {shop:"康师傅的围裙店",img:"images/TB2_zmFc80kpuFjy1XaXXaFkVXa_!!2106620341.jpg_80x80.jpg",cont:"ikbc poker升级版/新版poker2 60%mini机械键盘 樱桃轴红茶青黑",info1:"套餐类型：套餐一",info2:"颜色分类：Poker 白黑",pride2:"459.00",number:"1"},
+// {shop:"薛文鹏的镐把子店",img:"images/T244FzX2laXXXXXXXX-21171835.jpg_80x80.jpg",cont:"加厚隔热防晒现代美式纯色窗帘布全遮光定制卧室客厅落地飘窗成品",info1:"尺寸：宽2高2.6米 布袋挂钩一片",info2:"浅藕色[暮光]",pride1:"280.00",pride2:"162.40",number:"1"},
+// {shop:"薛文鹏的镐把子店",img:"images/T2kbOrXhlXXXXXXXXX_!!21171835.jpg_80x80.jpg",cont:"咖啡色现代简约美式纱帘定做客厅卧室书房飘窗纯色透光窗帘纱帘",info1:"尺寸：定制要几米拍几件（不含加工费）",info2:"颜色分类：网眼纱 白色",pride1:"109.00",pride2:"59.08",number:"3"},
+// {shop:"嘤嘤怪的大茬粥店",img:"images/TB2A9iAeWmWBuNjy1XaXXXCbXXa_!!1718249255.jpg_80x80.jpg",cont:"凯酷K10机械键盘樱桃轴108键87键PBT侧刻键4帽法拉利红键线分离",info1:"套餐类型：套餐一",info2:"颜色分类：87经典黑配DOLCH 侧刻",pride1:"899.00",pride2:"429.40",number:"1"},
+// ]
+
+var data=[{shop:"薛文鹏的镐把子店",datainfo:[{classify:"part1",img:"images/TB2QBzwmFXXXXbCXXXXXXXXXXXX_!!21171835.jpg_80x80.jpg",cont:"加厚隔热防晒现代美式纯色窗帘布全遮光定制卧室客厅落地飘窗成品",info1:"尺寸：宽2高2.6米 布袋挂钩一片",info2:"浅灰色[暮光]",pride1:"￥280.00",pride2:"162.40",number:"1",info3:"",info4:""},
+									{classify:"part1",img:"images/T244FzX2laXXXXXXXX-21171835.jpg_80x80.jpg",cont:"加厚隔热防晒现代美式纯色窗帘布全遮光定制卧室客厅落地飘窗成品",info1:"尺寸：宽2高2.6米 布袋挂钩一片",info2:"浅藕色[暮光]",pride1:"￥280.00",pride2:"162.40",number:"1",info3:"",info4:""},
+									{classify:"part1",img:"images/T2kbOrXhlXXXXXXXXX_!!21171835.jpg_80x80.jpg",cont:"咖啡色现代简约美式纱帘定做客厅卧室书房飘窗纯色透光窗帘纱帘",info1:"尺寸：定制要几米拍几件（不含加工费）",info2:"颜色分类：网眼纱 白色",pride1:"￥109.00",pride2:"59.08",number:"3",info3:"",info4:""}]},
+		{shop:"加多宝的袜子店",datainfo:[{classify:"part2",img:"images/TB2Q7i.vSVmpuFjSZFFXXcZApXa_!!2785160894.jpg_80x80.jpg",cont:"加厚美式棉麻全遮光窗帘隔音隔热客厅卧室窗帘布北欧纯色现代简约",info1:"尺寸：不加工/每米价格",info2:"黄色",pride1:"￥132.00",pride2:"66",number:"10",info3:"",info4:""}]},
+		{shop:"康师傅的围裙店",datainfo:[{classify:"part3",img:"images/TB2_zmFc80kpuFjy1XaXXaFkVXa_!!2106620341.jpg_80x80.jpg",cont:"ikbc poker升级版/新版poker2 60%mini机械键盘 樱桃轴红茶青黑",info1:"套餐类型：套餐一",info2:"颜色分类：Poker 白黑",pride1:"",pride2:"459.00",number:"1",info3:"",info4:""}]},
+		{shop:"嘤嘤怪的大茬粥店",datainfo:[{classify:"part4",img:"images/TB2A9iAeWmWBuNjy1XaXXXCbXXa_!!1718249255.jpg_80x80.jpg",cont:"凯酷K10机械键盘樱桃轴108键87键PBT侧刻键4帽法拉利红键线分离",info1:"套餐类型：套餐一",info2:"颜色分类：87经典黑配DOLCH 侧刻",pride1:"￥899.00",pride2:"429.40",number:"1",info3:"",info4:""}]},
+		{shop:"农夫山泉的豆腐乳店",datainfo:[{classify:"part5",img:"images/TB14qtKnMmTBuNjy1XbL6SMrVXa_80x80.jpg",cont:"【可领用100元单品券】Meizu/魅族 魅族 15 PLUS 全网通公开版4G",info1:"网络类型：4G全网通",info2:"机身颜色：玄武灰",info3:"套餐类型：官方标配",info4:"存储容量：6+64GB",pride1:"",pride2:"2498.00",number:"1"}]}]
+var contnum=0
+var pride_total=0
+
+var data_1=data.slice(0)
+var arr=[]
+for(var k=0;k<data.length;k++){
+	arr.push(document.createElement('div'))
+	arr[k].className="shop"
+	main.appendChild(arr[k])
+}
+var hes=main.getElementsByClassName('shop')
+var jiesuan1=document.getElementById("jiesuan")
+	for(var k1=0;k1<hes.length;k1++){
+	main.insertBefore(hes[k1],jiesuan1)
+}
+
+data.forEach(function(item,index){
+	var str1=""
+	str1=`<h3><i class="shopmark"></i>${item.shop}</h3>`
+	data[index].datainfo.forEach(function(item,index){
+		str1+=`<ul>
+				<li>
+					<i class="commark ${item.classify}"></i>
+					<div class="shangpintu"><img src=${item.img} alt=""></div>
+					<p class="shangpinming">${item.cont}</p>
+					<p class="shangpinxinxi"><span>${item.info1}</span><span>${item.info2}</span><span>${item.info3}</span><span>${item.info4}</span></p>	
+					<p class="shangpinjiage"><span class="zheqian">${item.pride1}</span><span class="zhehou">￥${item.pride2}</span></p>
+					<div class="shangpinshuliang"><i class="jian1"></i><i class="jia"></i><i class="jian">-</i><input type="text" value="${item.number}"><i class="jia">+</i></div>	
+					<p class="zhanshijiage">￥${item.number*item.pride2}</p>
+					<b class="shanchu">删除</b>
+				</li>
+			</ul>`
+	
+	})
+	hes[index].innerHTML=str1
+
+})
+
+var shopmark=document.getElementsByClassName('shopmark')
+var commark=document.getElementsByClassName('commark')
+for(let k3=0;k3<commark.length;k3++){
+	commark[k3].setAttribute("flag","false")
+}
+for(let k4=0;k4<shopmark.length;k4++){
+	shopmark[k4].setAttribute("flag","false")
+	shopmark[k4].setAttribute("index",k4)
+	shopmark[k4].onclick=function(ev){
+		var thispride=this.parentNode.parentNode.getElementsByClassName('zhanshijiage')
+			if(ev.target.getAttribute("flag")=="false"){
+			var thisnum1=this.parentNode.parentNode.getElementsByTagName('input')
+			ev.target.style.background="red"
+			ev.target.setAttribute("flag","true")
+			var commark_sin=hes[ev.target.getAttribute("index")].getElementsByClassName('commark')
+			for(var k5=0;k5<commark_sin.length;k5++){
+				commark_sin[k5].style.background="red"
+				commark_sin[k5].setAttribute("flag","true")
+			}
+			for(var k18=0;k18<thisnum1.length;k18++){
+			if(thisnum1[k18].parentNode.parentNode.firstElementChild.getAttribute("flag")=="true"){
+				contnum+=parseFloat(thisnum1[k18].value)
+				pride_total+=parseFloat(parseFloat(thispride[k18].innerHTML.replace("￥","")).toFixed(2))
+			}
+		}	if(pride_total<0){
+			pride_total=0
+		}
+			numtotal.innerHTML=contnum
+			pride.innerHTML=pride_total.toFixed(2)
+		}else{
+			var thisnum2=this.parentNode.parentNode.getElementsByTagName('input')
+			ev.target.style.background=""
+			ev.target.setAttribute("flag","false")
+			var commark_sin=hes[ev.target.getAttribute("index")].getElementsByClassName('commark')
+			
+
+			for(var k18=0;k18<thisnum2.length;k18++){
+			if(thisnum2[k18].parentNode.parentNode.firstElementChild.getAttribute("flag")=="true"){
+				contnum-=parseFloat(thisnum2[k18].value)
+				pride_total-=parseFloat(parseFloat(thispride[k18].innerHTML.replace("￥","")).toFixed(2))
+			}
+		}
+			for(var k5=0;k5<commark_sin.length;k5++){
+				commark_sin[k5].style.background=""
+				commark_sin[k5].setAttribute("flag","false")
+			}
+			if(pride_total<0){
+			pride_total=0
+		}
+			numtotal.innerHTML=contnum
+			pride.innerHTML=pride_total.toFixed(2)
+		}
+		
+		
+	}
+}
+
+
+var box=document.getElementsByClassName("shopmark")
+for(var k6=0;k6<commark.length;k6++){
+	commark[k6].onclick=function(ev){
+		var arr=[]
+		var that=this
+		var commark_classname=ev.target.getAttribute("class")
+		if(ev.target.getAttribute("flag")=="false"){
+			ev.target.setAttribute("flag","true")
+ 			ev.target.style.background="red"
+ 			contnum+=parseFloat(this.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.lastElementChild.previousElementSibling.value)
+ 			numtotal.innerHTML=contnum
+ 			pride_total+=parseFloat(this.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.lastElementChild.previousElementSibling.value)*parseFloat(parseFloat(this.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.lastElementChild.innerHTML.replace("￥","")).toFixed(2))
+ 			if(pride_total<0){
+			pride_total=0
+		}
+ 			pride.innerHTML=pride_total.toFixed(2)
+		}else{
+			ev.target.setAttribute("flag","false")
+ 			ev.target.style.background=""
+ 			contnum-=parseFloat(this.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.lastElementChild.previousElementSibling.value)
+ 			numtotal.innerHTML=contnum
+ 			pride_total-=parseFloat(this.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.lastElementChild.previousElementSibling.value)*parseFloat(parseFloat(this.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.lastElementChild.innerHTML.replace("￥","")).toFixed(2))
+ 			if(pride_total<0){
+			pride_total=0
+		}
+ 			pride.innerHTML=pride_total.toFixed(2)
+		}
+
+		for(var k7=0;k7<commark.length;k7++){
+			if(commark[k7].getAttribute("class")==commark_classname){
+				arr=arr.concat(commark[k7])}}
+		for(var k9=0;k9<arr.length;k9++){
+			if(arr[k9].getAttribute("flag")=="true"){
+				that.parentNode.parentNode.parentNode.firstElementChild.firstElementChild.style.background="red";
+				that.parentNode.parentNode.parentNode.firstElementChild.firstElementChild.setAttribute("flag","true");
+				return
+			}
+		}
+		that.parentNode.parentNode.parentNode.firstElementChild.firstElementChild.style.background="";
+		that.parentNode.parentNode.parentNode.firstElementChild.firstElementChild.setAttribute("flag","false");
+		
+	}
+}
+
+
+
+// for(var k17=0;k17<commark.length;k17++){
+// 			if(commark[k17].getAttribute("flag")=="false"){
+// 			quanxuan.previousElementSibling.style.background=""
+// 			quanxuan.previousElementSibling.setAttribute("flag","false");
+// 			return
+// 			}
+// 		}
+// 		quanxuan.previousElementSibling.style.background="red"
+// 		quanxuan.previousElementSibling.setAttribute("flag","true");
+
+
+
+var uls=document.getElementsByTagName('ul')
+for(var k11=0;k11<uls.length;k11++){
+	uls[k11].setAttribute("index",k11)
+}
+
+
+var subtraction=document.getElementsByClassName("jian")
+var plus=document.getElementsByClassName("jia")
+var zhehous=document.getElementsByClassName('zhehou')
+var zhanshijiages=document.getElementsByClassName('zhanshijiage')
+var num=document.getElementsByTagName('input')
+for(var k10=0;k10<subtraction.length;k10++){
+	subtraction[k10].onclick=function(ev){
+		var that=this
+		this.nextElementSibling.value--
+		if(this.parentNode.parentNode.firstElementChild.getAttribute("flag")=="true"){
+		contnum--
+		pride_total-=parseFloat(parseFloat(this.parentNode.previousElementSibling.lastElementChild.innerHTML.replace("￥","")).toFixed(2))
+		if(pride_total<0){
+			pride_total=0
+		}
+		pride.innerHTML=pride_total.toFixed(2)
+		numtotal.innerHTML=contnum
+		}
+		if(this.nextElementSibling.value==0){
+			this.style.display="none"
+		}else{
+			this.style.display="block"
+		}
+		var pride3=that.parentNode.previousElementSibling.lastElementChild.innerHTML.replace("￥","")
+		that.parentNode.nextElementSibling.innerHTML=`￥${(that.nextElementSibling.value*pride3).toFixed(2)}`
+	}
+}
+
+
+for(var k11=0;k11<plus.length;k11++){
+	plus[k11].onclick=function(ev){
+		if(this.parentNode.parentNode.firstElementChild.getAttribute("flag")=="true"){
+		contnum++
+		pride_total+=parseFloat(parseFloat(this.parentNode.previousElementSibling.lastElementChild.innerHTML.replace("￥","")).toFixed(2))
+		if(pride_total<0){
+			pride_total=0
+		}
+		pride.innerHTML=pride_total.toFixed(2)
+		numtotal.innerHTML=contnum
+		}
+		this.previousElementSibling.previousElementSibling.style.display="block"
+		var that=this
+		this.previousElementSibling.value++
+		var pride3=that.parentNode.previousElementSibling.lastElementChild.innerHTML.replace("￥","")
+		that.parentNode.nextElementSibling.innerHTML=`￥${(that.previousElementSibling.value*pride3).toFixed(2)}`
+	}
+}
+
+for(var k12=0;k12<num.length;k12++){
+	num[k12].onblur=function(ev){
+		if(this.value>0&&this.value!=""){
+			this.previousElementSibling.style.display="block"
+			var pride3=this.parentNode.previousElementSibling.lastElementChild.innerHTML.replace("￥","")
+			this.parentNode.nextElementSibling.innerHTML=`￥${(this.value*pride3).toFixed(2)}`
+		}else if(this.value<=0){
+			this.value=0
+			var pride3=this.parentNode.previousElementSibling.lastElementChild.innerHTML.replace("￥","")
+			this.parentNode.nextElementSibling.innerHTML=`￥${(this.value*pride3).toFixed(2)}`
+			this.previousElementSibling.style.display="none"
+		}
+		numtotal.innerHTML=contnum
+	}
+}
+
+var shanchu=document.getElementsByClassName('shanchu')
+for(var k13=0;k13<shanchu.length;k13++){
+	shanchu[k13].onclick=function(ev){
+		this.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode)
+		contnum-=this.previousElementSibling.previousElementSibling.lastElementChild.previousElementSibling.value
+		pride_total-=parseFloat(parseFloat(this.previousElementSibling.innerHTML.replace("￥","")).toFixed(2))
+		if(pride_total<0){
+			pride_total=0
+		}
+		pride.innerHTML=pride_total.toFixed(2)
+		numtotal.innerHTML=contnum
+		for(var k14=0;k14<hes.length;k14++){
+			var hes_ul=hes[k14].getElementsByTagName("ul")
+			if(hes_ul.length==0){
+				hes[k14].innerHTML=""
+			}
+		}
+	}
+}
+// quanxuan.previousElementSibling.setAttribute("flag","false")
+// quanxuan.onclick=function(ev){
+// 	if(ev.target.previousElementSibling.getAttribute("flag")=="false"){
+// 	ev.target.previousElementSibling.style.background="red"
+// 	ev.target.previousElementSibling.setAttribute("flag","true")
+// 	for(var k15=0;k15<shopmark.length;k15++){
+// 		shopmark[k15].style.background="red"
+// 		shopmark[k15].setAttribute("flag","true")
+// 	}
+// 	for(var k16=0;k16<commark.length;k16++){
+// 		commark[k16].style.background="red"
+// 		commark[k16].setAttribute("flag","true")
+// 	}
+// 	}else{
+// 		ev.target.previousElementSibling.style.background=""
+// 	ev.target.previousElementSibling.setAttribute("flag","false")
+// 	for(var k15=0;k15<shopmark.length;k15++){
+// 		shopmark[k15].style.background=""
+// 		shopmark[k15].setAttribute("flag","false")
+// 	}
+// 	for(var k16=0;k16<commark.length;k16++){
+// 		commark[k16].style.background=""
+// 		commark[k16].setAttribute("flag","false")
+// 	}
+// 	}
+// }
